e12cfdb9ae4f5df967132f636f19b08218fbab88
Valeriia Ushakova <ushakova94@yandex.ru>
2024-03-30 18:36:38 +0300

    fixed: name in function createCard and logic in function deleteCard

--- scripts/index.js
+++ scripts/index.js
@@ -3,31 +3,24 @@ const template = document.querySelector('#card-template').content;
 // @todo: DOM узлы
 const placesList = document.querySelector('.places__list');
 // @todo: Функция создания карточки
-function createCard(cardInfo, cardDelete) {
+function createCard(cardInfo, removeCard) {
   const newCard = template.querySelector('.places__item').cloneNode(true);
   const cardImage = newCard.querySelector('.card__image');
   cardImage.src = cardInfo.link;
+  cardImage.alt = cardInfo.name;
   const cardTitle = newCard.querySelector('.card__title');
   cardTitle.textContent = cardInfo.name;
 
   const cardDeleteButton = newCard.querySelector('.card__delete-button');
-  cardDeleteButton.addEventListener('click', cardDelete);
-
+  cardDeleteButton.addEventListener('click', () => removeCard(newCard));
   return newCard;
 }
 // @todo: Функция удаления карточки
-function deleteCard (evt) {
-  const buttonParent = evt.target.parentElement;
-  buttonParent.remove();
+function deleteCard (cardElemeent) {
+  cardElemeent.remove();
 };
 // @todo: Вывести карточки на страницу
-for (let i = 0; i < initialCards.length; i++) {
-  const newListItem = createCard(initialCards[i], deleteCard);
-  placesList.append(newListItem);
-};
-
-/*
 initialCards.forEach(function(item) {
   const newListItem = createCard(item, deleteCard);
   placesList.append(newListItem);
-}); */
+}); 
